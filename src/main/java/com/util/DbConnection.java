a71d37f36ba1b947313f4ade5afd89890d77ce86
Ayush Thakkar <ayushthakkar@Ayushs-Laptop.local>
2024-10-26 17:45:33 +0530

    first project

---
+++ src/main/java/com/util/DbConnection.java
@@ -0,0 +1,25 @@
+package com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection 
+{
+	public static Connection getConnection()
+	{
+		Connection conn = null;
+		String dbUrl = "";
+		String name = "root";
+		String password = "12345678";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbUrl, name, password);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
